2789cc09dd723fc9111f0b7652f0b1ec19f0b660
thanadol-git <khunkoei@gmail.com>
2025-01-02 07:25:01 +0100

    add sidebar

--- web.py
+++ web.py
@@ -4,8 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Create a sidebar
+st.sidebar.header("Sidebar")
+st.sidebar.write("This is the sidebar content.")
+
+
 # Create two tabs
-dia_tab, dda_tab = st.tabs(["DIA", "DDA"])
+dia_tab, dda_tab, srm_tab = st.tabs(["DIA", "DDA", "SRM"])
 
 # Content for Tab 1
 with dia_tab:
@@ -16,4 +21,11 @@ with dia_tab:
 with dda_tab:
     st.header("FE DDA")
     st.write("This is the content of the second tab.")
+
+# Content fo t Tab 3 
+
+with srm_tab:
+    st.header("FE SRM")
+    st.write("This is the content of the third tab.")
+
 
