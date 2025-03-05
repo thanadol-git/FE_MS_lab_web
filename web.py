32472a9f380f595d43aa716d9d2a8837453c6280
thanadol-git <khunkoei@gmail.com>
2025-03-05 14:01:58 +0100

    path

--- web.py
+++ web.py
@@ -8,7 +8,7 @@ import plotly.express as px
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
-with open('../config.yaml') as file:
+with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Create a sidebar
