d9122c95ec43b440a6124b15c515bd2dc29a37dd
thanadol-git <khunkoei@gmail.com>
2025-03-05 14:00:49 +0100

    launcher

--- web.py
+++ web.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
 # Create a sidebar
 st.sidebar.header("Sample information")
 st.sidebar.write("This part is needed for every file that we are creating..")
