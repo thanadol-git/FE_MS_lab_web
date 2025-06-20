af1abf7d8dd8c122089ed5b1143911905fdd2576
thanadol-git <khunkoei@gmail.com>
2025-06-20 19:03:07 +0200

    add split

--- web.py
+++ web.py
@@ -5,40 +5,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 import plotly.express as px
+from sidebar import create_sidebar
 
 
-# Create a sidebar
-st.sidebar.header("Sample information")
-st.sidebar.write("This part is needed for every file that we are creating.")
-
-# Project
-proj_name = st.sidebar.text_input("Enter your project name", "Project X")
-# Organism
-organism = st.sidebar.text_input("Select your organism", "Human")
-# Sample type
-sample = st.sidebar.text_input("Enter your sample", "Plasma")
-# Plate id 
-plate_id = st.sidebar.text_input("Enter your plate ID", "")
-# Samplee/ cohort name
-sample_name = st.sidebar.text_input("Main cohort name/abbreviation", "Cohort_1")
-# Instrument
-machine = st.sidebar.selectbox("Select your instrument", ["Q Exactive HF", "TSQ Altis", "LIT Stellar"])
-# Acquisition technique 
-acq_tech = st.sidebar.selectbox("Select your acquisition", ["DDA", "DIA", "SRM"])
-# if machine == "Q Exactive HF":
-#     acq_tech = "DIA"
-# elif machine == "TSQ Altis":
-#     acq_tech = "SRM"
-# else:
-#     acq_tech = "DIA"
-    
-if 'SRM' in acq_tech: # If SRM  do 
-    # acq_tech = "SRM"
-    srm_lot = st.sidebar.text_input('ProteomeEdge Lot number: Lot ', "23233")
-    if srm_lot: 
-        st.sidebar.markdown(f"The ProteomEdge Lot <span style='color:red'>{srm_lot}</span>", unsafe_allow_html=True)
-
-
+# Sidebar
+proj_name, organism, sample, plate_id, sample_name, machine, acq_tech = create_sidebar()
 
 
 # Create three tabs
