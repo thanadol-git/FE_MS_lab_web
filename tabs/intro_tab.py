f8ad86287403e1432bdf32b604b4d8bb5cf63100
thanadol-git <khunkoei@gmail.com>
2025-12-05 09:50:32 +0100

    fix link

--- tabs/intro_tab.py
+++ tabs/intro_tab.py
@@ -7,5 +7,5 @@ def intro_detail():
     st.write("Please fill in the necessary information in the sidebar to get started.")
     st.subheader("Related links")
     st.markdown("1. [Xcalibur](https://www.thermofisher.com/order/catalog/product/OPTON-30900)")
-    st.markdown("2. [EVOSEP Chronos](https://www.evosep.com/wp-content/uploads/2025/06/UM-004A-Evosep-Eno-Software-Installation-Guide-for-CHRONOS.pdf)")
+    st.markdown("2. [EVOSEP One](https://www.evosep.com/wp-content/uploads/2024/03/Evosep-One-User-Guide-v20-1.pdf)")
     st.markdown("3. [ProteomEdge SRM kit](https://proteomedge.com/?utm_source=google&utm_medium=cpc&utm_campaign=Brand&utm_id=21365304429&gad_source=1&gad_campaignid=21365304429&gbraid=0AAAAA9y8PtBsyDttZ6yULoNWs-UmrJ6ag&gclid=CjwKCAjw3tzHBhBREiwAlMJoUghlTAMhy4TIMGH_yulPSZRXZtj4zmd0FYp-ACp-5Xp6mMaQxh_AWBoCefUQAvD_BwE)")
